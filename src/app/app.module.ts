939ae5f2a131a4887a81700e09f273406403c54b
tanmh <tostack@163.com>
2020-03-05 17:26:14 +0800

    preview

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgLive2dModule } from 'ng-live2d';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgLive2dModule
   ],
   providers: [],
   bootstrap: [AppComponent]
